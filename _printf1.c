48152493fe3b15e78c6ff1b66a40b56a9f0c0898
MucheruM <petermucheru420@gmail.com>
2023-10-15 22:56:52 -0700

    Format the _printf code

--- _printf1.c
+++ _printf1.c
@@ -5,39 +5,41 @@
 
 int _printf(const char *format, ...);
 {
-va_list arg;
-va_start(arg, format);
-char_tally = 0;
-int x;
-for (x = 0; format[x] != '\0'; x++)
-{
-if (format[x] == '\0')
-{
-break;
-}
-switch (format(x));
-case "character"
-{
-char ch = va_arg(agr, int);
-write(1, &ch, 1);
-char_tally++;
-break;
-}
-case "string"
-{
-char *strg = va_arg(arg, int);
-write(1, strg, 1);
-char_tally++;
-break;
-}
-case "%"
-{
-write(1, "%", 1);
-char_tally++;
-break;
-}
-va_end(arg);
-return (char_tally);
-}
-}
+	va_list arg;
+	int x, char_tally = 0;
+	char ch, *strg;
+
+	va_start(arg, format);
+	for (x = 0; format[x] != '\0'; x++)
+	{
+		if (format[x] == '\0')
+		{
+			return (1);
+		}
+		switch (format(x))
+		{
+		case "character"
+		{
+			ch = va_arg(agr, int);
+			write(1, &ch, 1);
+			char_tally++;
+			break;
+		}
+		case "string"
+		{
+			*strg = va_arg(arg, int);
+			write(1, strg, 1);
+			char_tally++;
+			break;
+		}
+		case "%"
+		{
+			write(1, "%", 1);
+			char_tally++;
+			break;
+		}
+		}
+	}
+	va_end(arg);
+	return (char_tally);
 }
