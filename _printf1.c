a8de2a6831a7a796004c15f5bcca8391c131fabf
MucheruM <petermucheru420@gmail.com>
2023-10-16 10:56:07 -0700

    Format the function

--- _printf1.c
+++ _printf1.c
@@ -9,7 +9,7 @@
 */
 int _putchar(char c)
 {
-return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
 
 /**
@@ -21,54 +21,42 @@ return write(1, &c, 1);
 */
 int _printf(const char *format, ...)
 {
-va_list args;
-int tally = 0;
-const char *format_pos = format;
-va_start(args, format);
-while (*format_pos != '\0')
-{
-if (*format_pos == '%')
-{
-format_pos++;
+	va_list args;
+	int c, tally = 0;
+	const char *s, *format_pos = format;
 
-switch (*format_pos)
-{
-case 'c':
-{
-int c = va_arg(args, int);
-tally += _putchar(c);
-break;
-}
-case 's':
-{
-const char *s = va_arg(args, const char *);
-while (*s != '\0')
-{
-tally += _putchar(*s);
-s++;
-}
-break;
-}
-case '%':
-{
-tally += _putchar('%');
-break;
-}
-default:
-tally += _putchar('%');
-tally += _putchar(*format_pos);
-break;
-}
-}
-else
-{
-tally += _putchar(*format_pos);
-}
-
-format_pos++;
-}
-
-va_end(args);
-
-return (tally);
+	va_start(args, format);
+	while (*format_pos != '\0')
+	{
+		if (*format_pos == '%')
+		{
+			format_pos++;
+			switch (*format_pos)
+			{
+			case 'c':
+				c = va_arg(args, int);
+				tally += _putchar(c);
+				break;
+			case 's':
+				s = va_arg(args, const char *);
+				while (*s != '\0')
+				{
+					tally += _putchar(*s);
+					s++;
+				}
+				break;
+			case '%':
+				tally += _putchar('%');
+				break;
+			default:
+				tally += _putchar('%');
+				tally += _putchar(*format_pos);
+				break;
+			}
+		}
+		tally += _putchar(*format_pos);
+		format_pos++;
+	}
+	va_end(args);
+	return (tally);
 }
