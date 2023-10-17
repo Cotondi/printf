650822a3ab8f04bb2d4bd7d6edb930dc728edff6
MucheruM <petermucheru420@gmail.com>
2023-10-17 15:10:41 -0700

    Add structure and alignment to the code

--- _printf1.c
+++ _printf1.c
@@ -1,17 +1,6 @@
 #include "main.h"
 
 /**
-* _putchar - Write a character to stdout.
-* @c: The character to be written.
-*
-* Return: 1 (success) or -1 (error).
-*/
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
-/**
 * _printf - A streamlined version of the printf function.
 * @format: A character string that includes format placeholders.
 * @...: A flexible number of arguments.
@@ -20,52 +9,42 @@ return (write(1, &c, 1));
 */
 int _printf(const char *format, ...)
 {
-va_list args;
-int tally = 0;
-const char *format_pos = format;
-va_start(args, format);
+	va_list args;
+	int c, tally = 0;
+	const char *s, *format_pos = format;
 
-while (*format_pos != '\0')
-{
-if (*format_pos == '%')
-{
-format_pos++;
-
-switch (*format_pos)
-{
-case 'c':
-{
-int c = va_arg(args, int);
-tally += _putchar(c);
-break;
-}
-case 's':
-{
-const char *s = va_arg(args, const char *);
-while (*s)
-{
-tally += _putchar(*s);
-s++;
-}
-break;
-}
-case '%':
-tally += _putchar('%');
-break;
-default:
-tally += _putchar('%');
-tally += _putchar(*format_pos);
-break;
-}
-}
-else
-{
-tally += _putchar(*format_pos);
-}
-format_pos++;
-}
-
-va_end(args);
-
-return tally;
+	va_start(args, format);
+	while (*format_pos != '\0')
+	{
+		if (*format_pos == '%')
+		{
+			format_pos++;
+			switch (*format_pos)
+			{
+			case 'c':
+				c = va_arg(args, int);
+				tally += _putchar(c);
+				break;
+			case 's':
+				s = va_arg(args, const char *);
+				while (*s)
+				{
+					tally += _putchar(*s);
+					s++;
+				}
+				break;
+			case '%':
+				tally += _putchar('%');
+				break;
+			default:
+				tally += _putchar('%');
+				tally += _putchar(*format_pos);
+				break;
+			}
+			tally += _putchar(*format_pos);
+		}
+		format_pos++;
+	}
+	va_end(args);
+	return tally;
 }
