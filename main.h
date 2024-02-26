db88edf2ea481b8cf53d7136cf9ccaef398e86a3
MucheruM <petermucheru420@gmail.com>
2024-02-26 10:53:17 -0800

    Create a file holding prototypes for the projects

--- main.h
+++ main.h
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <stdarg.h>
 int _printf(const char *format, ...);
+int binary(int num);
+int octal(int num);
 int _putchar(char c);
 char *obtain_string(va_list args);
 int obtain_width(const char *format, va_list args);
